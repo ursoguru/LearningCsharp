e0a8e3d4f85256d43a1ac3de27dc52a4041fb842
ursoguru <garydaccarter@gmail.com>
2023-02-08 22:55:27 +0200

    Hullo World 2

---
+++ DelegatesSOL/Delegates/Classes/HulloWorld.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Delegates.Classes
+{
+	public class HulloWorld
+	{
+		public HulloWorld()
+		{
+            Console.WriteLine("Constructor");
+        }
+        public void Greet()
+        {
+            Console.Write("Hello World!");
+        }
+    }
+}
+
--- DelegatesSOL/Delegates/Program.cs
+++ DelegatesSOL/Delegates/Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!!!!");
+// Console.WriteLine("Hello, World!!!!");
+
+using Delegates.Classes;
+
+HulloWorld hulloWorld = new HulloWorld();
+
+hulloWorld.Greet();
+
 
